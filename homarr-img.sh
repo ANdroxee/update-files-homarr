9178ce4de157793dac41e1b8936148cdf69292a2
cedev-1 <121963179+cedev-1@users.noreply.github.com>
2024-03-07 11:07:41 +0100

    homarr-img.sh

--- homarr-img.sh
+++ homarr-img.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+echo "Example -> ./homarr_img.sh cedev@10.10.1.0 4fad5d4267aa /home/cedev/"
+
 mkdir -p img-homarr
 cp -r ./* img-homarr
 rm -rf ./img-homarr/img-homarr
 
-ssh cedev@192.168.1.124 "docker exec 4fad5d4267aa sh -c 'rm -rf /var/www/html/*'"
+# $1 -> SSH connection string (user@10.10.1.0)
+# $2 -> Docker container ID (4fad5d4267aa)
+# $3 -> File path on the remote server (/home/user/)
 
-scp -r img-homarr cedev@192.168.1.124:/home/cedev/
-ssh cedev@192.168.1.124 "docker cp /home/cedev/img-homarr/. 4fad5d4267aa:/app/public/imgs/"
-ssh cedev@192.168.1.124 "rm -rf /home/cedev/img-homarr"
+scp -r img-homarr $1:$3
+ssh $1 "docker cp $3/img-homarr/. $2:/app/public/imgs/"
+ssh $1 "rm -rf $3/img-homarr"
 rm -rf img-homarr
